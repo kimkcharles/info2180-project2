2ae1317bd72e0897a37024f40a658f35a780489e
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2018-11-02 17:15:33 +0000

    Rewrote code to position the puzzle pieces and background image

--- fifteen.js
+++ fifteen.js
@@ -1,32 +1,47 @@
 $(document).ready(function(){
 
-	pieces = $.makeArray($("#puzzlearea").children());
+	var pieces = $("#puzzlearea").children();
 
-
-	for (i=0;i<pieces.length;i++){
+	var count=0;
+	var x=0; //Left
+	var y=0; //Right
+	
+	
+	for (var i=0; i<pieces.length-1; i++){
+		//Adding "puzzlepiece" class to the tiles
 		pieces[i].classList.add("puzzlepiece");
+		//Positioning the tiles correctly
+		pieces[i].style.left= `${x}px`;
+		pieces[i].style.top= `${y}px`;
+		//Positioning the background image with respect to each tile
+		pieces[i].style.backgroundPosition = `-${x}px -${y}px`;
+		x+=100;
+		count+=1;
+		if(count%4==0){
+			y+=100;
+			x=0;
+		};
+		
+	};
+	
+	//All spaces that can be occupied in the puzzle
+	var allspaces=[];
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			allspaces.push([i*100,j*100]);
+		};
+	};
+	
+	function shuffle(){
+		let free_spaces=allspaces.slice();
+		pieces.forEach(function(e){
+			this.style.left= `${x}px`;
+			this.style.top= `${y}px`;
+			this.style.backgroundPosition = `-${x}px -${y}px`;
+			
+		}
+		);
 	}
 	
-	//Set pieces and the background image 
-	for (i=0;i<4;i++){
-		pieces[i].style.top                = "0px";
-		pieces[i].style.left               = `${100*i}px`;
-		pieces[i].style.backgroundPosition = `${-100*i}px 0px`;
-	}
-	for (i=4;i<8;i++){
-		pieces[i].style.top                = "100px";
-		pieces[i].style.left               = `${100*(i-4)}px`;
-		pieces[i].style.backgroundPosition = ` ${-100*(i-4)}px -100px`
-	}
-	for (i=8;i<12;i++){
-		pieces[i].style.top                = "200px";
-		pieces[i].style.left               = `${100*(i-8)}px`;
-		pieces[i].style.backgroundPosition = `${-100*(i-8)}px -200px`
-	}
-	for (i=12;i<15;i++){
-		pieces[i].style.top                = "300px";
-		pieces[i].style.left               = `${100*(i-12)}px`;
-		pieces[i].style.backgroundPosition = `${-100*(i-12)}px -300px`
-	}
-	
+
 })
