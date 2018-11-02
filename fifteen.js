4e7b77cea255c3923228157e5599d3565a0b68fa
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2018-11-02 22:55:55 +0000

    Working on the shuffling

--- fifteen.js
+++ fifteen.js
@@ -6,8 +6,13 @@ $(document).ready(function(){
 	var x=0; //Left
 	var y=0; //Right
 	
+	/*function setbkgrd(tile){
+		let xb=parseInt(tile.style.left);
+		let yb=parseInt(tile.style.top);
+		tile.style.backgroundPosition = `-${xb}px -${yb}px`;
+	}*/
 	
-	for (var i=0; i<pieces.length-1; i++){
+	for (var i=0; i<pieces.length; i++){
 		//Adding "puzzlepiece" class to the tiles
 		pieces[i].classList.add("puzzlepiece");
 		//Positioning the tiles correctly
@@ -26,22 +31,47 @@ $(document).ready(function(){
 	
 	//All spaces that can be occupied in the puzzle
 	var allspaces=[];
+	var counta=0;
 	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			allspaces.push([i*100,j*100]);
+		for(let j=0;j<4;j++){
+			allspaces.push([i*100, j*100, counta]);
+			counta+=1;
 		};
 	};
 	
+	
+	
 	function shuffle(){
 		let free_spaces=allspaces.slice();
-		pieces.forEach(function(e){
-			this.style.left= `${x}px`;
-			this.style.top= `${y}px`;
-			this.style.backgroundPosition = `-${x}px -${y}px`;
+		//let usedtile=[];
+		let ftile;
+		function shuff(e){
+			
+			let c=free_spaces.length;
+			let ind=Math.floor(Math.random() * Math.floor(c));
+			ftile=free_spaces[ind];
+			
+			//Assign the piece a free tile
+			let xs= ftile[0];
+			let ys= ftile[1];
+			//let ind= ftile[2];
+			e.style.left= `${xs}px`;
+			e.style.top= `${ys}px`;
+			
+			free_spaces.splice(ind,1);
 			
 		}
-		);
+		for (var i=0; i<pieces.length; i++){
+			let p=pieces[i];
+			shuff(p);
+		};
+		
 	}
 	
+	//$("#shufflebutton").click(shuffle());
+	
+	//document.getElementById("shufflebutton").onclick =shuffle();
 
 })
+
+
