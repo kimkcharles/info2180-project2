471029cba11ee462ae02ae94abb38c6cd5efe3a3
Kimberly <kimmy_khamille@yahoo.com>
2018-11-06 16:35:05 -0500

    Final; completed

--- fifteen.js
+++ fifteen.js
@@ -1,16 +1,11 @@
 $(document).ready(function(){
-
+//Extra Feature: Random background image is chosen by the game on startup and the player can change the background image
 	var pieces = Array.from($("#puzzlearea").children());
 
 	var count=0;
 	var x=0; //Left
 	var y=0; //Right
 	
-	/*function setbkgrd(tile){
-		let xb=parseInt(tile.style.left);
-		let yb=parseInt(tile.style.top);
-		tile.style.backgroundPosition = `-${xb}px -${yb}px`;
-	}*/
 	
 	for (var i=0; i<pieces.length; i++){
 		//Adding "puzzlepiece" class to the tiles
@@ -49,14 +44,6 @@ $(document).ready(function(){
 		return [parseInt(`${left}`),parseInt(`${top}`)];
 	}
 
-
-	//console.log(parseInt(`${pieces[1].style.left}`));
-	/*
-	pieces.forEach(function(e){
-		console.log(tileLocation(e));
-	})
-	//*/
-
 	function findBlankSpace() {
 		let emptyTile=[];
 		let free_spaces=allspaces.slice();
@@ -64,26 +51,13 @@ $(document).ready(function(){
 		occupiedLocations = pieces.map(function(tile){
 			return tileLocation(tile);
 		})
-		//console.log(`occupied are`);
-		//console.log(occupiedLocations);
-		//console.log(`matching`);
-		//console.log(free_spaces);
+		
+
 		free_spaces.forEach(function(location){
-			//console.log(location);
-			/*
-    		if(!(containsElement(occupiedLocations,location))){
-    			emptyTile = location;
-    		}
-			*/
-			///*
-			
 			occupied=false;
 			for(i=0; i<occupiedLocations.length; i++){
 				
 				if(occupiedLocations[i][0]===location[0] && occupiedLocations[i][1]===location[1]){
-					//free_spaces.splice(i,1)
-					//let pp=`${occupiedLocations[i]} is equal to ${location} `;
-					///console.log(pp);
 					occupied=true;
 					break;
 				}else{
@@ -99,21 +73,7 @@ $(document).ready(function(){
 		return emptyTile;
 	}
 
-	//let whyy=findBlankSpace();
-	//console.log(whyy);
-
-	//console.log(findBlankSpace());
-	/*
-	$(".explanation").click(function(){
-		let whyy=findBlankSpace();
-		console.log(whyy);
-		alert(`${whyy} and piece 3 is left ${pieces[2].style.left} and top ${pieces[2].style.top}`);
-		
-	});
-	//*/
-
-
-
+	
 
 	function movablePiece(piece){
 		let tile=tileLocation(piece);
@@ -137,10 +97,8 @@ $(document).ready(function(){
 		
 	$(".explanation").click(function(){
 		let whyy=findBlankSpace();
-		//console.log(whyy);
 		alert(`blank space is ${whyy} and piece 3 is left ${pieces[2].style.left} and top ${pieces[2].style.top}`);
-		pieces.forEach(function(e){
-			//console.log(tileLocation(e));
+		pieces.forEach(function(e){			
 			let move=movablePiece(e);
 			console.log(`${tileLocation(e)} is movable: ${move}`);
 		})
@@ -148,11 +106,6 @@ $(document).ready(function(){
 
 	function moveInBlankSpace(tile){
 		let blankLocation = findBlankSpace();
-		//let pieceLocation = tileLocation(tile);	
-		//Swap location of empty tile and moving tile
-		//temp = emptyTileLocation;
-		//emptyTileLocation = pieceLocation;
-		//pieceLocation = temp;
 		let pieceLocation = findBlankSpace();
 
 		changeTileLocation(tile, pieceLocation[0],pieceLocation[1]);
@@ -196,84 +149,85 @@ $(document).ready(function(){
 		shuffle()
 	});
 	
-	//document.getElementById("shufflebutton").addEventListener("click",shuffle());
-	
 	function changeBackground(img){
 		pieces.forEach(function(tile){
 			tile.style.backgroundImage = "url('" +  img.name + "')";
 		})
 	}
-	//$('<div/>',{
-	//	'class': 'test',
-
-	//});
-	d = document.createElement('div');
-	$(d).addClass("cdc")
-    	.html("chiv")
-    	.appendTo($("#overall"))
-
-    kk = document.createElement('div');
-	$(kk).addClass("dc")
-    	.html("noo")
-    	.appendTo($("#overall"))
-
-    hilight_css = {"border-bottom-color":"red", 
-               "background-color":"coral"
-           		};
-	$(".cdc").css(hilight_css);
-	ilight_css = {"border-bottom-color":"red", 
-               "background-color":"coral"
-           		};
-	$(".dc").css(ilight_css);
+	
 
 	var backgrounds_box= $("<div></div>");
 	var instructions=$("<div></div>").text("Select the image that you wish to use.");
 	var background_options= $("<div></div>").addClass("rows");
-	var background_options_r1= $("<div></div>").addClass("r1");
-	var background_options_r1= $("<div></div>").addClass("r2");
-	var background1= $("<img></img>").addClass("bg").attr({"src":"background.jpg", "width":"80px", "height":"80px"});
-	var background2= $("<img></img>").addClass("bg").attr({"src":"bb2.jpg", "width":"80px", "height":"80px"});
-	var background3= $("<img></img>").addClass("bg").attr({"src":"bb3.jpg", "width":"80px", "height":"80px"});
-	var background4= $("<img></img>").addClass("bg").attr({"src":"bb4.jpg", "width":"80px", "height":"80px"});
-	//$("#overall").append(backgrounds_box);
-	//backgrounds_box.insertBefore($(#shufflebutton));
+	var background1= $("<img></img>").addClass("bg").attr({"src":"background.jpg" });
+
+	var background2= $("<img></img>").addClass("bg").attr({"src":"bb2.jpg" });
+	var background3= $("<img></img>").addClass("bg").attr({"src":"bb3.jpg" });
+	var background4= $("<img></img>").addClass("bg").attr({"src":"bb4.jpg" });
+	var background5= $("<img></img>").addClass("bg").attr({"src":"bb5.jpg" });
+	var background6= $("<img></img>").addClass("bg").attr({"src":"bb6.jpg" });
+	var background7= $("<img></img>").addClass("bg").attr({"src":"bb7.jpg" });
+	var background8= $("<img></img>").addClass("bg").attr({"src":"bb8.jpg" });
+	
 	let puzzl=$("#puzzlearea"); 
 	backgrounds_box.insertAfter(puzzl);
 	backgrounds_box.append(instructions);
 	backgrounds_box.append(background_options);
-	background_options.append(background1, background2, background3, background4);
-	
-	///*
+	background_options.append(background1, background2, background3, background4, background5, background6, background7, background8);
 	
 	
-	$(background_options).css({
-		"display": "grid",
-		"grid-template-columns": "80px 80px",
-		"grid-template-rows": "80px 80px"
-		//"grid-gap": "2px"
+	$(".bg").css({
+		"border":"2px solid coral",
+		"width":"96px",
+		"height":"96px"
+
 	})
+	
+	background_options.css({
+		"display": "grid",
+		"grid-template-columns": "repeat(4, 100px)",
+		"grid-template-rows": "repeat(2, 100px)"
+	})
+
+	 backgrounds_box.css({
+	 	"margin-top": "10px"
+	 })
 
 	instructions.css({
 		"background-color": "#9370DB",
-		"width": "400px",
+		"width": "398px",
 		"font-size" : "1.25em",
 		"text-align": "center",
 		"color": "white",
 		"border": "1.5px solid #9370DB"
 	});
-	//*/
+	
+	
+	var b_options=Array.from(background_options.children());
+	
+	function selectImage(im){
+		pieces.forEach(function(puzzlepiece){
+			puzzlepiece.style.backgroundImage = "url('" +  im.src + "')";
+		})
+	}
 
-	var txt2 = $("<p></p>").text("Text.");   
-	txt2.appendTo($("#overall"));
-	//$("#puzzlearea").css({"float":"left"});
-    //$("p").css("color", "red");
-    //var pij=$("#puzzlearea")
-    //pij.css.({"border-color": "red"});
-    //d.css.("float", "right");
-   // $("#shufflebutton").css.("clear","both");
+	function randomImg(){
+		let len=b_options.length;
+		let ri = Math.floor(Math.random()*len);
+		console.log(b_options[ri].src);
+		selectImage(b_options[ri]);
+		
+	}
+
+	randomImg();
 
 	
-	//var txt2 = $("<p></p>").text("Text."); 
+
+	b_options.forEach(function(im){
+		$(im).click(function(){
+			selectImage(im);
+		});
+	});
 	
 })
 
